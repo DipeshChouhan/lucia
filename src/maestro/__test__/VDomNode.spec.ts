341a2d59d6f1b95a9e9b77ce6801aa7e5f77ed5e
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-29 17:28:13 -0700

    tests: some more tests for vdomnode

--- src/maestro/__test__/VDomNode.spec.ts
+++ src/maestro/__test__/VDomNode.spec.ts
@@ -4,4 +4,35 @@ describe('VDomNode', () => {
   it('should create without errors', () => {
     new VDomNode('div');
   });
+  it('should return the key', () => {
+    const node = new VDomNode(
+      'div',
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      453
+    );
+    expect(node.key).toBe(453);
+    expect(node.uniqueKey()).toBe(453);
+  });
+  it('should return a random hash', () => {
+    const node = new VDomNode('div');
+    expect(node.key).toBe(undefined);
+    expect(node.uniqueKey()).toBe(84696352);
+  });
+  it('tainted should be false', () => {
+    const node = new VDomNode('div');
+    expect(node.isTainted()).toBe(false)
+  })
+  it('should append a child', () => {
+    const node = new VDomNode('div');
+    const child = new VDomNode('div');
+    node.appendChild(child);
+    expect(node.children).toEqual([child]);
+    expect(node.isTainted()).toBe(true);
+    expect(child.parent).toEqual(node);
+  });
 });
