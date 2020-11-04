29744eb331aa021625815c347fddfe780c05e65e
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-04 15:18:15 -0800

    feat: no longer using with

--- src/directives/__test__/html.spec.ts
+++ src/directives/__test__/html.spec.ts
@@ -3,7 +3,7 @@ import { htmlDirective } from '../html';
 describe('.htmlDirective', () => {
   it('should set the html', () => {
     const fakeElem = document.createElement('div');
-    htmlDirective(fakeElem, 'l-html', 'count', { count: 0 });
+    htmlDirective(fakeElem, 'l-html', 'this.count', { count: 0 });
     expect(fakeElem.innerHTML).toBe('0');
   });
 });
--- src/directives/__test__/text.spec.ts
+++ src/directives/__test__/text.spec.ts
@@ -3,7 +3,7 @@ import { textDirective } from '../text';
 describe('.textDirective', () => {
   it('should set the text content', () => {
     const fakeElem = document.createElement('div');
-    textDirective(fakeElem, 'l-text', "'$' + money", { money: 0 });
+    textDirective(fakeElem, 'l-text', "'$' + this.money", { money: 0 });
     expect(fakeElem.textContent).toBe('$0');
   });
 });
